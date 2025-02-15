dd7ff6a8c6234705e90e963a496990521fe6428d
Piotr Żywczak <123404938+zywczak@users.noreply.github.com>
2025-02-15 15:54:50 +0100

    create RegisterAPIView

---
+++ HydroponicSystem/HydroponicSystem_Authentication/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import RegisterUserAPIView
+
+urlpatterns = [
+    path('register/', RegisterUserAPIView.as_view()),    
+]
--- HydroponicSystem/HydroponicSystem_Authentication/views.py
+++ HydroponicSystem/HydroponicSystem_Authentication/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework import status
+from .serializers import UserRegisterSerializer
 
-# Create your views here.
+class RegisterUserAPIView(APIView):
+    serializer_class = UserRegisterSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            new_user = serializer.save()
+            if new_user:
+                return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
