d67d849de15c1763f6dd7cc93779c14e02797199
zywczak <pjzywczak@gmail.com>
2025-02-16 23:23:46 +0100

    fixing bug

--- HydroponicSystem/HydroponicSystem_Authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import RegisterUserAPIView, UserLoginAPIView
-
-urlpatterns = [
-    path('login/', UserLoginAPIView.as_view(), name='login'),
-    path('register/', RegisterUserAPIView.as_view(), name='register'),          
-]
